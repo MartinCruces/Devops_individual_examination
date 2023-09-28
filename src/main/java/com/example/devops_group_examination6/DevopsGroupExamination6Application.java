765f725e8b9aaadceb13d96ad84438556107df5c
Jonathan Hellgren <jonathan.hellgren7@gmail.com>
2023-09-28 10:40:32 +0200

    First Commit

---
+++ src/main/java/com/example/devops_group_examination6/DevopsGroupExamination6Application.java
@@ -0,0 +1,13 @@
+package com.example.devops_group_examination6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsGroupExamination6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsGroupExamination6Application.class, args);
+    }
+
+}
